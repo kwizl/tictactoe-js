e0294605a46d0662a88d5e51671981904fd605b2
rsnazario <rafael.s.nazario@gmail.com>
2020-08-12 16:21:49 -0300

    linter fix for board.js file

--- js/board.js
+++ js/board.js
@@ -1,10 +1,10 @@
 const Board = () => {
   const boardChoices = () => {
-    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     return array;
   };
 
   return { boardChoices };
 };
 
-export { Board }
+export default Board;
